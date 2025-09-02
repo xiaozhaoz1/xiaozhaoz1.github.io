c0b972e051ced06acc50cf2ce7537ab1d150d99b
xiaozhaoz1 <1466877808@qq.com>
2025-09-02 21:52:25 +0800

    Site updated: 2025-09-02 21:52:24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1756821021376";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1756821142089";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
