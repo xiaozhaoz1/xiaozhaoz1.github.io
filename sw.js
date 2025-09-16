b0c83ca4d3a868a7802a21df398f223a394ae906
xiaozhaoz1 <1466877808@qq.com>
2025-09-16 19:26:38 +0800

    Site updated: 2025-09-16 19:26:37

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1756821502319";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1758006914684";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
